a93b2d6bc82a3bd3b90ec0ccd7df0881a59029fb
Yuta HIGUCHI <y-higuchi@onlab.us>
2014-11-18 14:31:10 -0800

    Fix QuorumIndex calculation

--- core/src/main/java/net/kuujo/copycat/internal/replication/ClusterReplicator.java
+++ core/src/main/java/net/kuujo/copycat/internal/replication/ClusterReplicator.java
@@ -66,8 +66,9 @@ public class ClusterReplicator implements Replicator, Observer {
     if (writeQuorum < 1) {
       writeQuorum = state.config().getCommandQuorumStrategy().calculateQuorumSize(state.clusterManager().cluster());
     }
-    int quorumSize = (int) Math.floor((replicas.size() + 1) / 2) + 1;
-    quorumIndex = quorumSize > 1 ? quorumSize - 2 : 0; // Subtract two, one for the current node and one for list indices
+    final int clusterSize = replicas.size() + 1;
+    final int quorumSize = (int) Math.floor(clusterSize / 2) + 1;
+    quorumIndex = clusterSize - quorumSize;
   }
 
   @Override
